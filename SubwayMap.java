886d71d0be7de8c5b8b3e5e7246a2ca3a0780454
Hu ruixuan <2609621486@qq.com>
2024-05-20 12:36:45 +0800

    找出中转站功能已完成

---
+++ SubwayMap.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class SubwayMap {
+    private Map<String, Map<String, Double>> map;
+
+
+    public SubwayMap() {
+        this.map = new LinkedHashMap<>();
+    }
+
+
+    public void addLine(String lineName) {
+        map.put(lineName, new LinkedHashMap<>());
+    }
+
+    public void addStation(String lineName, String stationName, double distance) {
+        map.get(lineName).put(stationName, distance);
+    }
+
+    //获取所有中转站
+    public Set<String> getTransferStations() {
+        Map<String, Set<String>> stationLines = new HashMap<>();
+        for (String line : map.keySet()) {
+            for (String station : map.get(line).keySet()) {
+                stationLines.putIfAbsent(station, new HashSet<>());
+                stationLines.get(station).add(line);
+            }
+        }//找出路线上的交集元素
+
+        Set<String> transferStations = new HashSet<>();
+        for (String station : stationLines.keySet()) {
+            if (stationLines.get(station).size() > 1) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("<").append(station).append(", <");
+                for (String line : stationLines.get(station)) {
+                    sb.append(line).append(" 号线、");
+                }
+                sb.setLength(sb.length() - 1); // 删除最后的逗号
+                sb.append(">>");
+                transferStations.add(sb.toString());
+            }
+        }
+
+        return transferStations;
+    }
+
+    
+}
